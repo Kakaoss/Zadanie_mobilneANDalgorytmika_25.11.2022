8f42103fdd3c12d34c4d6f465f4a40dc50be2307
OGMaksiu <mateckimaksymilian@gmail.com>
2022-11-25 14:57:38 +0100

    xml zrobiony

--- app/src/main/java/com/barstool/zadanie_mobilnealgorytmika_25112022/MainActivity.kt
+++ app/src/main/java/com/barstool/zadanie_mobilnealgorytmika_25112022/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.barstool.zadanie_mobilnealgorytmika_25112022
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val losuj = findViewById<Button>(R.id.btnLosuj)
+        val sortuj = findViewById<Button>(R.id.btnSort)
+
+
+
     }
 }
