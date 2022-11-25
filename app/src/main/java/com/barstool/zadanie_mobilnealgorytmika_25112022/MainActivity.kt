6768dd83c7d179d39e97d5c8ba51504c967a0793
OGMaksiu <mateckimaksymilian@gmail.com>
2022-11-25 15:33:03 +0100

    losowanie skonczone

--- app/src/main/java/com/barstool/zadanie_mobilnealgorytmika_25112022/MainActivity.kt
+++ app/src/main/java/com/barstool/zadanie_mobilnealgorytmika_25112022/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Switch
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -15,7 +16,24 @@ class MainActivity : AppCompatActivity() {
         val losuj = findViewById<Button>(R.id.btnLosuj)
         val sortuj = findViewById<Button>(R.id.btnSort)
 
+        val przyciski:List<Switch> = listOf(
+            findViewById(R.id.switch1),
+            findViewById(R.id.switch2),
+            findViewById(R.id.switch3),
+            findViewById(R.id.switch4),
+            findViewById(R.id.switch5),
+            findViewById(R.id.switch6),
+            findViewById(R.id.switch7),
+            findViewById(R.id.switch8),
+            findViewById(R.id.switch9)
+        )
 
-
+    losuj.setOnClickListener {
+        var i = 0
+        while (i < 9){
+            przyciski[i].text = Random.nextInt(0,50).toString()
+            i += 1
+        }
+    }
     }
 }
